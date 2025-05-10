3896de93474fc021c96f27c21dee7e41092129ab
Huzyefah <huzyefahsaqib13@gmail.com>
2025-05-10 14:15:56 +0500

    .

--- server/app.js
+++ server/app.js
@@ -18,7 +18,10 @@ app.use((req, res, next) => {
 
 // MongoDB Connection
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/spacefleet";
-mongoose.connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("MongoDB connection error:", err));
 
@@ -38,9 +41,6 @@ app.use((err, req, res, next) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// Export for Vercel
-module.exports = app;
